f96d947c762509558cb6d01e680ee0bdc7dd1c35
Ömer Faruk Birgül <birgulo@itu.edu.tr>
2022-03-21 19:08:23 +0300

    update

--- problem_1_a.py
+++ problem_1_a.py
@@ -35,6 +35,7 @@ text_line = f_ciphertext_one_line_read.readlines()[0]
 
 f_occurs_all = open("occurs_all.txt", "w")
 f_occurs_multi = open("occurs_multi.txt", "w")
+f_factors = open("factors.txt", "w")
 
 # indices_object = (re.finditer(
 #     "GVJ", (f_ciphertext_one_line_read.readlines()[0])))
@@ -90,7 +91,7 @@ for j in range(3, 12):
 
 print(pattern_list)
 print(len(pattern_list))
-
+factors_list = []
 for pattern in pattern_list:
     # print(pattern)
     #     # pattern = text_line[i] + text_line[i + 1] + text_line[i + 2]
@@ -105,12 +106,32 @@ for pattern in pattern_list:
 #
     if len(indices) > 1:
         spacing = indices[1] - indices[0]
-        f_occurs_multi.write(pattern + " " + str(spacing) + " "
-                             + str(divisors(spacing)) + " "
-                             + str(indices) + "\n")
+        f_factors.write(str(divisors(spacing)) + "\n")
+        # f_occurs_multi.write(pattern + " " + str(spacing) + " "
+        #                      + str(divisors(spacing)) + " "
+        #                      + str(indices) + "\n")
+        factors_list.append(divisors(spacing))
     else:
         pass
 
+# print(factors_list)
+f_factors.close()
 f_occurs_all.close()
 f_occurs_multi.close()
 f_ciphertext_one_line_read.close()
+
+search_list = []
+p = 0
+for string in factors_list:
+    o = 0
+    for factors in factors_list[p]:
+        number = factors_list[p][o]
+        if number in search_list:
+            pass
+        else:
+            search_list.append(number)
+
+        o = o + 1
+    p = p + 1
+search_list.sort()
+print(search_list)
