c8d6ff3d68fddadbd1dd8da309ec12137a003f6b
Ömer Faruk Birgül <farukbrgl@protonmail.com>
2022-04-16 23:22:20 +0300

    update

--- project_2/problem_1/cipher_1.py
+++ project_2/problem_1/cipher_1.py
@@ -1,6 +1,12 @@
-def key_schedules():
-    key = [1] * 16
-    print(key)
+def key_schedules(key_hex):
+    print(key_hex)
+    key_list = map(bin, str(key_hex))
+    print(key_list)
+    key_15 = key_list[0] ^ key_list[2] ^ key_list[4] ^ key_list[5]
+    key_shifted = [key_15] + key_list[:-1]
+    print(key_shifted)
+
+    return key_shifted
 
 
 key_schedules()
